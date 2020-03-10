a4de694f48dc461a3c672c6c161d8a11175cdf2c
wwwins <jacky.huang@isobar.com>
2020-03-10 11:28:21 +0800

    Update for fastify-gm plugin

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@
 'use strict';
 
 const dotenv = require('dotenv').config();
-const path = require('path')
+const path = require('path');
+const uuid = require('uuid');
 
 const URL = process.env.URL;
 const PORT = (process.env.PORT || 8000);
@@ -18,9 +19,18 @@ const ENABLE_MONGO = MONGO_URI!="";
 const fastify = require('fastify')({
   logger: true
 });
+
 fastify.register(require('fastify-static'), {
   root: path.join(__dirname, '../public')
 });
+
+fastify.register(require('../modules/fastify-gm'), {
+  font: path.join(__dirname, '../fonts/NotoSansTC-Regular.otf'),
+  from_folder: path.join(__dirname, '../public/images/'),
+  to_folder: path.join(__dirname, '../public/upload/'),
+});
+
+fastify.register(require('fastify-no-icon'));
 
 fastify.get('/', async (req, res) => {
   return { hello: 'isobar' }
@@ -28,6 +38,16 @@ fastify.get('/', async (req, res) => {
 
 fastify.get('/user/:username', async (req, res) => {
   return { hello: req.params.username }
+})
+
+fastify.get('/jpg/:color/:text', async (req, res) => {
+  const text = req.params.text;
+  const color = req.params.color;
+  const uid = uuid();
+  if (text) {
+    await fastify.textOverImage({text:text, pos:{x:50,y:150}, color:color, fileId:uid});
+    return { success: true, jpg: API_HOST+'/upload/'+uid+'.jpg' }
+  }
 })
 
 const start = async () => {
